94d7a50086f8446cb89cad2ab763fcd98f03aa12
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-07-08 07:02:50 +0000

    [cmake] Add OpenCV to CMakeLists.txt
    Symptom: facelib requires OpenCV.
    Root cause: NA
    Solution: Add OpenCV to CMakeLists.txt.
    Note: NA
    
    Change-Id: Ie9b666dcba58a55cb2bf9d024f58ded8250e134d

---
+++ cmake/opencv.cmake
@@ -0,0 +1,30 @@
+# Copyright 2018 Bitmain Inc.
+# License
+# Author Yangwen Huang <yangwen.huang@bitmain.com>
+
+
+if("${OPENCV_ROOT}" STREQUAL "")
+  message(FATAL_ERROR "You must set OPENCV_ROOT before building library.")
+elseif(EXISTS "${OPENCV_ROOT}")
+  message("-- Found OPENCV_ROOT (directory: ${OPENCV_ROOT})")
+else()
+  message(FATAL_ERROR "${OPENCV_ROOT} is not a valid folder.")
+endif()
+
+set(OPENCV_INCLUDES
+    ${OPENCV_ROOT}/include/
+    ${OPENCV_ROOT}/include/opencv/
+)
+
+set(OPENCV_LIBS ${OPENCV_ROOT}/lib/libopencv_calib3d.so
+                ${OPENCV_ROOT}/lib/libopencv_features2d.so
+                ${OPENCV_ROOT}/lib/libopencv_flann.so
+                ${OPENCV_ROOT}/lib/libopencv_imgcodecs.so
+                ${OPENCV_ROOT}/lib/libopencv_imgproc.so
+                ${OPENCV_ROOT}/lib/libopencv_ml.so
+                ${OPENCV_ROOT}/lib/libopencv_core.so)
+
+if (NOT "${CMAKE_BUILD_TYPE}" STREQUAL "SDKRelease")
+  install(DIRECTORY ${OPENCV_ROOT}/include/ DESTINATION ${CMAKE_INSTALL_PREFIX}/include/opencv)
+  install(FILES ${OPENCV_LIBS} DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/)
+endif()
