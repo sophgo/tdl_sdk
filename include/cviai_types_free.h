1d0246d1df487a333df5e14450d691dc7905c693
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-07-10 08:10:05 +0000

    [API] Add missing extern "C"
    Symptom: Forget to add extern "C".
    Root cause: NA
    Solution: Add it.
    Note: NA
    
    Change-Id: Ib8c05f95d489855359dcf53528d582c94f40427d

--- include/cviai_types_free.h
+++ include/cviai_types_free.h
@@ -4,6 +4,10 @@
 #include "face/cvi_face_types.h"
 #include "object/cvi_object_types.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void CVI_AI_FreeFeature(cvi_feature_t *feature);
 void CVI_AI_FreePts(cvi_pts_t *pts);
 
@@ -12,4 +16,8 @@ void CVI_AI_FreeFace(cvi_face_t *face);
 
 void CVI_AI_FreeObjectInfo(cvi_object_info_t *face_info);
 void CVI_AI_FreeObject(cvi_object_t *face);
+
+#ifdef __cplusplus
+}
+#endif
 #endif
