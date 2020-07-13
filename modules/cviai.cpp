0d2008869ad8b31ccaa5b0abf2a0af96e794018f
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-07-13 03:25:21 +0000

    [clang-format] Do clang-format on cviai.cpp
    Change-Id: I7f94f6cb549a799c904cda26ef0df774cde66c5b

--- modules/cviai.cpp
+++ modules/cviai.cpp
@@ -1,7 +1,7 @@
-#include <memory>
-#include <vector>
-#include <string>
 #include <unistd.h>
+#include <memory>
+#include <string>
+#include <vector>
 
 #include "cviai.h"
 
@@ -11,24 +11,25 @@ using namespace std;
 using namespace cviai;
 
 typedef struct {
-    Yolov3 *yolov3;
+  Yolov3 *yolov3;
 } cviai_context_t;
 
 int CVI_AI_InitHandle(const cviai_config_t *config, cviai_handle_t *handle) {
-    cviai_context_t *context = new cviai_context_t;
+  cviai_context_t *context = new cviai_context_t;
 
-    if (config->model_yolo3 != NULL) {
-        context->yolov3 = new Yolov3();
-        context->yolov3->modelOpen(config->model_yolo3);
-        printf("init_network_yolov3 done\n");
-    }
+  if (config->model_yolo3 != NULL) {
+    context->yolov3 = new Yolov3();
+    context->yolov3->modelOpen(config->model_yolo3);
+    printf("init_network_yolov3 done\n");
+  }
 
-    *handle = context;
-    return 0;
+  *handle = context;
+  return 0;
 }
 
-int CVI_AI_ObjDetect(cviai_handle_t handle, VIDEO_FRAME_INFO_S *stObjDetFrame, cvi_object_t *obj, cvi_obj_det_type_t det_type) {
-    cviai_context_t *fctx = static_cast<cviai_context_t*>(handle);
+int CVI_AI_ObjDetect(cviai_handle_t handle, VIDEO_FRAME_INFO_S *stObjDetFrame, cvi_object_t *obj,
+                     cvi_obj_det_type_t det_type) {
+  cviai_context_t *fctx = static_cast<cviai_context_t *>(handle);
 
-    return fctx->yolov3->inference(stObjDetFrame, obj, det_type);
+  return fctx->yolov3->inference(stObjDetFrame, obj, det_type);
 }
