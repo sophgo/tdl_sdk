0f8955c021bfd68be4597ebd4a9b819b86626b51
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-07-28 02:31:27 +0000

    [cmake] Fix tracer include path
    Symptom: Tracer include path is incorrect.
    Root cause: NA
    Solution: Fix it.
    Note: NA
    
    Change-Id: I83f946f0c5b376429f511931a71cb22230c4d599

--- cmake/tracer.cmake
+++ cmake/tracer.cmake
@@ -3,7 +3,7 @@
 # Author Yangwen Huang <yangwen.huang@bitmain.com>
 
 if("${TRACER_SDK_ROOT}" STREQUAL "")
-  message(FATAL_ERROR "You must set TRACER_SDK_ROOT before building IVE library.")
+  message(FATAL_ERROR "You must set TRACER_SDK_ROOT before building CVIAI library.")
 elseif(EXISTS "${TRACER_SDK_ROOT}")
   message("-- Found TRACER_SDK_ROOT (directory: ${TRACER_SDK_ROOT})")
 else()
@@ -12,7 +12,7 @@ endif()
 
 project(tracer-sdk)
 set(TRACER_INCLUDES
-    ${TRACER_SDK_ROOT}/include/
+    ${TRACER_SDK_ROOT}/include/tracer
 )
 
 set(TRACER_LIBS
@@ -20,6 +20,6 @@ set(TRACER_LIBS
 )
 
 if (NOT "${CMAKE_BUILD_TYPE}" STREQUAL "SDKRelease")
-  install(DIRECTORY ${TRACER_SDK_ROOT}/include/ DESTINATION ${CMAKE_INSTALL_PREFIX}/include/)
+  install(DIRECTORY ${TRACER_SDK_ROOT}/include/tracer DESTINATION ${CMAKE_INSTALL_PREFIX}/include/tracer)
   install(FILES ${TRACER_LIBS} DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/)
 endif()
