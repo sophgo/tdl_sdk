d4bfcd42baf084a8f8f14e707362b169bb1cf211
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-07-29 07:43:43 +0000

    [cviai] Add CVI_AI_Buffer2VBFrame API
    Symptom: All NN input in AI SDK use VBFrame.
    Root cause: OpenCV does not use VBFrame.
    Solution: Add a function to convert raw buffer image to VBFrame.
    Note: NA
    
    Change-Id: Ife1548b3a88e758f042cbe3648a579e4e1032572

--- modules/cviai.cpp
+++ modules/cviai.cpp
@@ -144,6 +144,86 @@ int CVI_AI_CloseModel(cviai_handle_t handle, CVI_AI_SUPPORTED_MODEL_E config) {
   return CVI_SUCCESS;
 }
 
+inline void BufferRGBPackedCopy(const uint8_t *buffer, uint32_t width, uint32_t height,
+                                uint32_t stride, VIDEO_FRAME_INFO_S *frame, bool invert) {
+  VIDEO_FRAME_S *vFrame = &frame->stVFrame;
+  if (invert) {
+    for (uint32_t j = 0; j < height; j++) {
+      const uint8_t *ptr = buffer + j * stride;
+      for (uint32_t i = 0; i < width; i++) {
+        uint32_t offset = i * 3 + j * vFrame->u32Stride[0];
+        const uint8_t *ptr_pxl = i * 3 + ptr;
+        vFrame->pu8VirAddr[0][offset] = ptr_pxl[2];
+        vFrame->pu8VirAddr[0][offset + 1] = ptr_pxl[1];
+        vFrame->pu8VirAddr[0][offset + 2] = ptr_pxl[0];
+      }
+    }
+  } else {
+    for (uint32_t j = 0; j < height; j++) {
+      const uint8_t *ptr = buffer + j * stride;
+      for (uint32_t i = 0; i < width; i++) {
+        uint32_t offset = i * 3 + j * vFrame->u32Stride[0];
+        const uint8_t *ptr_pxl = i * 3 + ptr;
+        vFrame->pu8VirAddr[0][offset] = ptr_pxl[0];
+        vFrame->pu8VirAddr[0][offset + 1] = ptr_pxl[1];
+        vFrame->pu8VirAddr[0][offset + 2] = ptr_pxl[2];
+      }
+    }
+  }
+}
+
+inline void BufferRGBPacked2PlanarCopy(const uint8_t *buffer, uint32_t width, uint32_t height,
+                                       uint32_t stride, VIDEO_FRAME_INFO_S *frame, bool invert) {
+  VIDEO_FRAME_S *vFrame = &frame->stVFrame;
+  if (invert) {
+    for (uint32_t j = 0; j < height; j++) {
+      const uint8_t *ptr = buffer + j * stride;
+      for (uint32_t i = 0; i < width; i++) {
+        const uint8_t *ptr_pxl = i * 3 + ptr;
+        vFrame->pu8VirAddr[0][i + j * vFrame->u32Stride[0]] = ptr_pxl[0];
+        vFrame->pu8VirAddr[1][i + j * vFrame->u32Stride[1]] = ptr_pxl[1];
+        vFrame->pu8VirAddr[2][i + j * vFrame->u32Stride[2]] = ptr_pxl[2];
+      }
+    }
+  } else {
+    for (uint32_t j = 0; j < height; j++) {
+      const uint8_t *ptr = buffer + j * stride;
+      for (uint32_t i = 0; i < width; i++) {
+        const uint8_t *ptr_pxl = i * 3 + ptr;
+        vFrame->pu8VirAddr[0][i + j * vFrame->u32Stride[0]] = ptr_pxl[2];
+        vFrame->pu8VirAddr[1][i + j * vFrame->u32Stride[1]] = ptr_pxl[1];
+        vFrame->pu8VirAddr[2][i + j * vFrame->u32Stride[2]] = ptr_pxl[0];
+      }
+    }
+  }
+}
+
+int CVI_AI_Buffer2VBFrame(const uint8_t *buffer, uint32_t width, uint32_t height, uint32_t stride,
+                          const PIXEL_FORMAT_E inFormat, VB_BLK *blk, VIDEO_FRAME_INFO_S *frame,
+                          const PIXEL_FORMAT_E outFormat) {
+  if (CREATE_VBFRAME_HELPER(blk, frame, width, height, outFormat) != CVI_SUCCESS) {
+    printf("Create VBFrame failed.\n");
+    return CVI_FAILURE;
+  }
+
+  if ((inFormat == PIXEL_FORMAT_RGB_888 && outFormat == PIXEL_FORMAT_BGR_888) ||
+      (inFormat == PIXEL_FORMAT_BGR_888 && outFormat == PIXEL_FORMAT_RGB_888)) {
+    BufferRGBPackedCopy(buffer, width, height, stride, frame, true);
+  } else if ((inFormat == PIXEL_FORMAT_RGB_888 && outFormat == PIXEL_FORMAT_RGB_888) ||
+             (inFormat == PIXEL_FORMAT_BGR_888 && outFormat == PIXEL_FORMAT_BGR_888)) {
+    BufferRGBPackedCopy(buffer, width, height, stride, frame, false);
+  } else if (inFormat == PIXEL_FORMAT_BGR_888 && outFormat == PIXEL_FORMAT_RGB_888_PLANAR) {
+    BufferRGBPacked2PlanarCopy(buffer, width, height, stride, frame, true);
+  } else if (inFormat == PIXEL_FORMAT_RGB_888 && outFormat == PIXEL_FORMAT_RGB_888_PLANAR) {
+    BufferRGBPacked2PlanarCopy(buffer, width, height, stride, frame, false);
+  } else {
+    printf("Unsupported convert format: %u -> %u.\n", inFormat, outFormat);
+    return CVI_FAILURE;
+  }
+
+  return CVI_SUCCESS;
+}
+
 int CVI_AI_ReadImage(const char *filepath, VB_BLK *blk, VIDEO_FRAME_INFO_S *frame,
                      PIXEL_FORMAT_E format) {
   cv::Mat img = cv::imread(filepath);
