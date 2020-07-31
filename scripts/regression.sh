3abd6088971be118e8f26baa67d5c2ace1d45f0d
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-07-31 07:09:15 +0000

    [scripts] Change regression script behavior
    Symptom: Need to print true/ false.
    Root cause: Daniel said.
    Solution: Change to print result.
    Note: NA
    
    Change-Id: Ib8455905da66538f6e9ec8de4d6ecd980d611994

--- scripts/regression.sh
+++ scripts/regression.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+run() {
+    result="PASSED"
+    ./$2 || result="FAILED"
+    echo "TEST $1 $2: $result"
+}
 # For normal CI check
-init || exit 1
+CI_PROGRAMS=(init)
+
+for ((i=0; i < ${#CI_PROGRAMS[@]}; i++))
+do
+    run $i ${CI_PROGRAMS[$i]}
+done
 
 # For daily build tests
 if [[ "$1" != "daily" ]]; then
