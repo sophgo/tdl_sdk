7b3ba8e1a4db5ebdb2823d0d1e1d8013d0efc55d
Terry Tang <terry.tang@bitmain.com>
2020-08-05 13:48:34 +0800

    Add frr/far evaluation in reg_face_attribute.
    Change-Id: I3e53a1d1f5e354370bbef02908c01a544c378257

--- regression/reg_face_attribute.c
+++ regression/reg_face_attribute.c
@@ -8,9 +8,14 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <cvimath/cvimath.h>
+
 #include "cviai.h"
 #include "sample_comm.h"
 #include "core/utils/vpss_helper.h"
+
+#define NUM_DATA        1000
+#define FEATURE_LENGTH  512
 
 cviai_handle_t facelib_handle = NULL;
 
@@ -69,6 +74,87 @@ static int genFeatureFile(const char *img_name_list, const char *feature_dir) {
   return CVI_SUCCESS;
 }
 
+static int evaluateResult()
+{
+  int8_t db_feature[NUM_DATA*FEATURE_LENGTH];
+  int8_t in_db_feature[NUM_DATA*FEATURE_LENGTH];
+  int8_t not_db_feature[NUM_DATA*FEATURE_LENGTH];
+  for (int i = 0; i < NUM_DATA; i++) {
+    char db_path[50];
+    char in_db_path[50];
+    char not_db_path[50];
+    sprintf(db_path, "/mnt/data/db_feature/%d.txt", i);
+    sprintf(in_db_path, "/mnt/data/in_db_feature/%d.txt", i);
+    sprintf(not_db_path, "/mnt/data/not_db_feature/%d.txt", i);
+
+    FILE *fp_db;
+    FILE *fp_in;
+    FILE *fp_not;
+    if((fp_db = fopen(db_path, "r")) == NULL) {
+      printf("file open error!");
+      return CVI_FAILURE;
+    }
+    if((fp_in = fopen(in_db_path, "r")) == NULL) {
+      printf("file open error!");
+      return CVI_FAILURE;
+    }
+    if((fp_not = fopen(not_db_path, "r")) == NULL) {
+      printf("file open error!");
+      return CVI_FAILURE;
+    }
+
+    int line = 0;
+    int idx = 0;
+    while(fscanf(fp_db, "%d\n", &line) != EOF) {
+      db_feature[i*FEATURE_LENGTH + idx] = line;
+      idx++;
+    }
+    idx = 0;
+    while(fscanf(fp_in, "%d\n", &line) != EOF) {
+      in_db_feature[i*FEATURE_LENGTH + idx] = line;
+      idx++;
+    }
+    idx = 0;
+    while(fscanf(fp_not, "%d\n", &line) != EOF) {
+      not_db_feature[i*FEATURE_LENGTH + idx] = line;
+      idx++;
+    }
+
+    fclose(fp_db);
+    fclose(fp_in);
+    fclose(fp_not);
+    printf("%d\n", i);
+  }
+
+  float db_f[NUM_DATA*FEATURE_LENGTH];
+  cvm_gen_db_i8_unit_length(db_feature, db_f, FEATURE_LENGTH, NUM_DATA);
+
+  float threshold = 0.41;
+  int frr = 0;
+  int far = 0;
+  for (int i = 0; i < NUM_DATA; i++) {
+    unsigned int k_index[NUM_DATA] = {0};
+    float k_value[NUM_DATA] = {0};
+    float buffer[NUM_DATA*FEATURE_LENGTH];
+    cvm_cpu_i8data_ip_match(&in_db_feature[i*FEATURE_LENGTH], db_feature, db_f, k_index, k_value, buffer,
+                            FEATURE_LENGTH, NUM_DATA, 1);
+    if (k_value[0] < threshold) frr++;
+  }
+  for (int i = 0; i < NUM_DATA; i++) {
+    unsigned int k_index[NUM_DATA] = {0};
+    float k_value[NUM_DATA] = {0};
+    float buffer[NUM_DATA*FEATURE_LENGTH];
+    cvm_cpu_i8data_ip_match(&not_db_feature[i*FEATURE_LENGTH], db_feature, db_f, k_index, k_value, buffer,
+                            FEATURE_LENGTH, NUM_DATA, 1);
+    if (k_value[0] > threshold) far++;
+  }
+
+  printf("frr: %d\n", frr);
+  printf("far: %d\n", far);
+
+  return 0;
+}
+
 int main(void) {
   CVI_S32 ret = CVI_SUCCESS;
 
@@ -96,8 +182,11 @@ int main(void) {
 
   CVI_AI_SetSkipVpssPreprocess(facelib_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, false);
 
-  genFeatureFile("/mnt/data/probe_name.txt", "/mnt/data/probe_feature/");
-  genFeatureFile("/mnt/data/gallery_name.txt", "/mnt/data/gallery_feature/");
+  genFeatureFile("/mnt/data/db_name.txt", "/mnt/data/db_feature/");
+  genFeatureFile("/mnt/data/in_db_name.txt", "/mnt/data/in_db_feature/");
+  genFeatureFile("/mnt/data/not_db_name.txt", "/mnt/data/not_db_feature/");
 
   CVI_AI_DestroyHandle(facelib_handle);
+
+  evaluateResult();
 }
