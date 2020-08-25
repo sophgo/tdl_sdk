09811069958a4922b9e8c32f73488a59b5fb7de4
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-08-25 08:56:15 +0000

    [core_utils] Add box_rescale_rb
    Symptom: Padding may pad right & bottom.
    Root cause: NA
    Solution: Add box_rescale_rb.
    Note: NA
    
    Change-Id: Ib0171791d2a14470ae5325abe380025d704c4935

--- modules/core/object_detection/yolov3/yolov3.cpp
+++ modules/core/object_detection/yolov3/yolov3.cpp
@@ -163,8 +163,8 @@ void Yolov3::outputParser(VIDEO_FRAME_INFO_S *srcFrame, cvai_object_t *obj,
   }
   if (!m_skip_vpss_preprocess) {
     for (int i = 0; i < obj->size; ++i) {
-      obj->info[i].bbox = box_rescale(srcFrame->stVFrame.u32Width, srcFrame->stVFrame.u32Height,
-                                      obj->width, obj->height, obj->info[i].bbox);
+      obj->info[i].bbox = box_rescale_c(srcFrame->stVFrame.u32Width, srcFrame->stVFrame.u32Height,
+                                        obj->width, obj->height, obj->info[i].bbox);
     }
     obj->width = srcFrame->stVFrame.u32Width;
     obj->height = srcFrame->stVFrame.u32Height;
