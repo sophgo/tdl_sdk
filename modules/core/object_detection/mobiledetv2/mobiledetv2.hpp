c00dd4ff3000cd161e07c3c8a972c73a4a399186
Jeff Huang <jiaji.huang@cvitek.com>
2020-09-02 16:00:46 +0800

    [cviai] change mobiledetv2 threshold to 0.4
    Symptom: N/A
    Root cause: N/A
    Solution: N/A
    Note: N/A
    Change-Id: I9e0a88082c129c94e76a02280d6505b08af75ac7

--- modules/core/object_detection/mobiledetv2/mobiledetv2.hpp
+++ modules/core/object_detection/mobiledetv2/mobiledetv2.hpp
@@ -45,7 +45,7 @@ class MobileDetV2 final : public Core {
     int label;
   };
 
-  MobileDetV2(MobileDetV2::Model model, float iou_thresh = 0.45, float score_thresh = 0.3);
+  MobileDetV2(MobileDetV2::Model model, float iou_thresh = 0.45, float score_thresh = 0.4);
   virtual ~MobileDetV2();
   int inference(VIDEO_FRAME_INFO_S *frame, cvai_object_t *meta, cvai_obj_det_type_t det_type);
 
