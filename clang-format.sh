f0fda53d1f4fcede3f5b998ec54c3c57f58fe8d6
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-09-03 02:08:24 +0000

    [clang-format] Add support for Ubuntu 20.04 docker image
    Symptom: NA
    Root cause: Ubuntu 20.04 uses clang-10.
    Solution: Add support to shell script.
    Note: NA
    
    Change-Id: Idd2db82c7b0d86252129b4b2230af47228d34253

--- clang-format.sh
+++ clang-format.sh
@@ -2,6 +2,13 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CLANG_ROOT=$(readlink -f $SCRIPT_DIR)
 
-find $CLANG_ROOT/include -regex '.*\.\(cpp\|h\|hpp\|cc\|c\|cxx\|inc\)' | xargs clang-format-5.0 -i || exit 1
-find $CLANG_ROOT/modules -regex '.*\.\(cpp\|h\|hpp\|cc\|c\|cxx\|inc\)' | xargs clang-format-5.0 -i || exit 1
-find $CLANG_ROOT/sample -regex '.*\.\(cpp\|h\|hpp\|cc\|c\|cxx\|inc\)' | xargs clang-format-5.0 -i || exit 1
+OS_VERSION=$(cat /etc/os-release | grep VERSION_ID)
+if [[ $OS_VERSION == *"20"* ]]; then
+  CLANG_FORMAT=clang-format-10
+else
+  CLANG_FORMAT=clang-format-5.0
+fi
+
+find $CLANG_ROOT/include -regex '.*\.\(cpp\|h\|hpp\|cc\|c\|cxx\|inc\)' | xargs $CLANG_FORMAT -i || exit 1
+find $CLANG_ROOT/modules -regex '.*\.\(cpp\|h\|hpp\|cc\|c\|cxx\|inc\)' | xargs $CLANG_FORMAT -i || exit 1
+find $CLANG_ROOT/sample -regex '.*\.\(cpp\|h\|hpp\|cc\|c\|cxx\|inc\)' | xargs $CLANG_FORMAT -i || exit 1
