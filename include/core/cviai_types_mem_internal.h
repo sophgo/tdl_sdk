054b5c6850502ff4c2c86d1ea4d29d277a3c629b
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-09-28 02:05:44 +0000

    [cviai] Add featurePtrConvert2Float helper function
    Symptom: This function converts types to float array.
    Root cause: NA
    Solution: NA
    Note: NA
    
    Change-Id: Ia1303528bf8013dfebc61e5eb70e5e1873f3d6cd

--- include/core/cviai_types_mem_internal.h
+++ include/core/cviai_types_mem_internal.h
@@ -1,5 +1,6 @@
 #ifndef _CVIAI_TYPES_MEM_INTERNAL_H_
 #define _CVIAI_TYPES_MEM_INTERNAL_H_
+#include "cviai_log.hpp"
 
 inline void CVI_AI_MemAlloc(const uint32_t unit_len, const uint32_t size, const feature_type_e type,
                             cvai_feature_t *feature) {
@@ -19,6 +20,70 @@ inline void CVI_AI_MemAlloc(const uint32_t size, cvai_pts_t *pts) {
     pts->y = (float *)malloc(sizeof(float) * size);
     pts->size = size;
   }
+}
+
+inline void __attribute__((always_inline))
+featurePtrConvert2Float(cvai_feature_t *feature, float *output) {
+  switch (feature->type) {
+    case TYPE_INT8: {
+      int8_t *ptr = (int8_t *)(feature->ptr);
+      for (uint32_t i = 0; i < feature->size; ++i) {
+        output[i] = ptr[i];
+      }
+    } break;
+    case TYPE_UINT8: {
+      uint8_t *ptr = (uint8_t *)(feature->ptr);
+      for (uint32_t i = 0; i < feature->size; ++i) {
+        output[i] = ptr[i];
+      }
+    } break;
+    case TYPE_INT16: {
+      int16_t *ptr = (int16_t *)(feature->ptr);
+      for (uint32_t i = 0; i < feature->size; ++i) {
+        output[i] = ptr[i];
+      }
+    } break;
+    case TYPE_UINT16: {
+      uint16_t *ptr = (uint16_t *)(feature->ptr);
+      for (uint32_t i = 0; i < feature->size; ++i) {
+        output[i] = ptr[i];
+      }
+    } break;
+    case TYPE_INT32: {
+      int32_t *ptr = (int32_t *)(feature->ptr);
+      for (uint32_t i = 0; i < feature->size; ++i) {
+        output[i] = ptr[i];
+      }
+    } break;
+    case TYPE_UINT32: {
+      uint32_t *ptr = (uint32_t *)(feature->ptr);
+      for (uint32_t i = 0; i < feature->size; ++i) {
+        output[i] = ptr[i];
+      }
+    } break;
+    case TYPE_BF16: {
+      uint16_t *ptr = (uint16_t *)(feature->ptr);
+      union {
+        float a;
+        uint16_t b[2];
+      } bfb;
+      bfb.b[0] = 0;
+      for (uint32_t i = 0; i < feature->size; ++i) {
+        bfb.b[1] = ptr[i];
+        output[i] = bfb.a;
+      }
+    } break;
+    case TYPE_FLOAT: {
+      float *ptr = (float *)(feature->ptr);
+      for (uint32_t i = 0; i < feature->size; ++i) {
+        output[i] = ptr[i];
+      }
+    } break;
+    default:
+      LOGE("Unsupported type: %u.\n", feature->type);
+      break;
+  }
+  return;
 }
 
 #endif  // End of _CVIAI_TYPES_MEM_INTERNAL_H_
