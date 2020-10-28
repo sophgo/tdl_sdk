2256d75274f0e182d35cd5ca47308a1536997500
Terry Tang <terry.tang@cvitek.com>
2020-10-28 11:05:18 +0800

    Add pc tools for regression evaluation.
    Change-Id: Ia3d7f26f001d24f3d51c46772a3c1dff3ebfebf9

---
+++ regression/pc_tools/README.md
@@ -0,0 +1,20 @@
+# Regression PC tools
+
+Python tools for evaluate algorithm result.
+
+## How to run
+
+### Environment
+
+1. python 3.6
+2. pycocotools
+3. octave 4.4.1
+4. mlir repo
+
+### Command
+
+```
+$ python3 eval_coco.py ~/data/coco/instances_val2017.json ~/data/coco/result.json
+$ python3 eval_thermal.py ~/data/thermal_val/annotations.json ~/data/thermal_val/result.json
+$ python3 eval_widerface.py ~/data/wider_face/wider_result ~/mlir/python
+```
---
+++ regression/pc_tools/eval_coco.py
@@ -0,0 +1,19 @@
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('annotation', metavar='annotation', help='Annotations json file path')
+    parser.add_argument('result', metavar='result', help='results json path')
+    args = parser.parse_args()
+
+    coco_gt = COCO(args.annotation)
+    coco_dt = coco_gt.loadRes(args.result)
+    # img_ids = sorted(coco_gt.getImgIds())
+    coco_eval = COCOeval(coco_gt, coco_dt, 'bbox')
+
+    # coco_eval.params.imgIds = img_ids
+    coco_eval.evaluate()
+    coco_eval.accumulate()
+    coco_eval.summarize()
