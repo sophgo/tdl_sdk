ffb1f364b63689204c13cce1cd71701640eec7f4
Jeff Huang <jiaji.huang@cvitek.com>
2021-07-09 20:55:28 +0800

    [build] remove BUILD_TYPE args from build script
    Symptom: N/A
    Root cause: N/A
    Solution: N/A
    Note: N/A
    Change-Id: I0d68e037bcc1345a28214992530bbd91d2baf969
    Reviewed-on: https://10.58.65.11:8087/c/cviai/+/11386
    Reviewed-by: <jiaji.huang@cvitek.com>

--- scripts/sdk_release.sh
+++ scripts/sdk_release.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CVIAI_ROOT=$(readlink -f $SCRIPT_DIR/../)
 TMP_WORKING_DIR=$CVIAI_ROOT/tmp
 
-if [[ "$2" == "Asan" ]]; then
+if [[ "$1" == "Asan" ]]; then
     BUILD_TYPE=Asan
 else
     BUILD_TYPE=SDKRelease
@@ -15,42 +15,30 @@ wget ftp://sa_admin:sa_admin@10.58.65.3//dependency_lib/prebuilt/cmake/cmake-3.1
 tar zxf cmake-3.18.4-Linux-x86_64.tar.gz
 CMAKE_BIN=$PWD/cmake-3.18.4-Linux-x86_64/bin/cmake
 echo "Creating tmp working directory."
-if [[ "$1" == "cmodel" ]]; then
-    echo "Temporarily not supported."
-elif [[ "$1" == "soc" ]]; then
-    $CMAKE_BIN -G Ninja $CVIAI_ROOT -DCVI_PLATFORM=$CHIP_ARCH \
-                                    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-                                    -DOPENCV_ROOT=$OPENCV_INSTALL_PATH \
-                                    -DMLIR_SDK_ROOT=$TPU_SDK_INSTALL_PATH \
-                                    -DMIDDLEWARE_SDK_ROOT=$MW_PATH \
-                                    -DIVE_SDK_ROOT=$IVE_SDK_INSTALL_PATH \
-                                    -DCMAKE_INSTALL_PREFIX=$AI_SDK_INSTALL_PATH \
-                                    -DTOOLCHAIN_ROOT_DIR=$HOST_TOOL_PATH \
-                                    -DCMAKE_TOOLCHAIN_FILE=$CVIAI_ROOT/toolchain/toolchain-aarch64-linux.cmake
-    ninja -j8 || exit 1
-    ninja install || exit 1
-elif [[ "$1" == "soc32" ]]; then
-    mkdir -p $TMP_WORKING_DIR/build_sdk
-    pushd $TMP_WORKING_DIR/build_sdk
-    if [[ "$SDK_VER" == "uclibc" ]]; then
-        TOOLCHAIN_FILE=$CVIAI_ROOT/toolchain/toolchain-uclibc-linux.cmake
-    else
-        TOOLCHAIN_FILE=$CVIAI_ROOT/toolchain/toolchain-gnueabihf-linux.cmake
-    fi
-    $CMAKE_BIN -G Ninja $CVIAI_ROOT -DCVI_PLATFORM=$CHIP_ARCH \
-                                    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-                                    -DOPENCV_ROOT=$OPENCV_INSTALL_PATH \
-                                    -DMLIR_SDK_ROOT=$TPU_SDK_INSTALL_PATH \
-                                    -DMIDDLEWARE_SDK_ROOT=$MW_PATH \
-                                    -DIVE_SDK_ROOT=$IVE_SDK_INSTALL_PATH \
-                                    -DCMAKE_INSTALL_PREFIX=$AI_SDK_INSTALL_PATH \
-                                    -DTOOLCHAIN_ROOT_DIR=$HOST_TOOL_PATH \
-                                    -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE
-    ninja -j8 || exit 1
-    ninja install || exit 1
+
+if [[ "$SDK_VER" == "uclibc" ]]; then
+    TOOLCHAIN_FILE=$CVIAI_ROOT/toolchain/toolchain-uclibc-linux.cmake
+elif [[ "$SDK_VER" == "64bit" ]]; then
+    TOOLCHAIN_FILE=$CVIAI_ROOT/toolchain/toolchain-aarch64-linux.cmake
+elif [[ "$SDK_VER" == "32bit" ]]; then
+    TOOLCHAIN_FILE=$CVIAI_ROOT/toolchain/toolchain-gnueabihf-linux.cmake
 else
-  echo "Unsupported build type."
+    echo "Wrong SDK_VER=$SDK_VER"
+    exit 1
 fi
+
+$CMAKE_BIN -G Ninja $CVIAI_ROOT -DCVI_PLATFORM=$CHIP_ARCH \
+                                -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+                                -DOPENCV_ROOT=$OPENCV_INSTALL_PATH \
+                                -DMLIR_SDK_ROOT=$TPU_SDK_INSTALL_PATH \
+                                -DMIDDLEWARE_SDK_ROOT=$MW_PATH \
+                                -DIVE_SDK_ROOT=$IVE_SDK_INSTALL_PATH \
+                                -DCMAKE_INSTALL_PREFIX=$AI_SDK_INSTALL_PATH \
+                                -DTOOLCHAIN_ROOT_DIR=$HOST_TOOL_PATH \
+                                -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE
+ninja -j8 || exit 1
+ninja install || exit 1
+
 popd
 echo "Cleanup tmp folder."
 rm -rf $TMP_WORKING_DIR
