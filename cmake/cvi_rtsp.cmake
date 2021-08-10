c02ec353a045bbb7a15a3840c116287cd1170a67
Jeff Huang <jiaji.huang@cvitek.com>
2021-08-10 22:09:19 +0800

    [rtsp] modify camke for new version of rtsp build script
    Change-Id: Ibc3b3ab7e49c4c50031eac793f61b84e2eab54bd
    Reviewed-on: https://gerritmaster.cvitek.com:8087/c/cviai/+/13115
    Reviewed-by: <jiaji.huang@cvitek.com>

--- cmake/cvi_rtsp.cmake
+++ cmake/cvi_rtsp.cmake
@@ -1,23 +1,10 @@
 include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
 include(FetchContent)
 
-if (ARCH STREQUAL "arm64")
-  set(LIVE555_URL ftp://10.34.33.5/prebuilt/easy_build/live555/live555_2020.08.12_cv183x.tar.xz)
-else()
-  set(LIVE555_URL ftp://10.34.33.5/prebuilt/easy_build/live555/live555_2020.08.12_cv183x_lib32.tar.xz)
-endif()
-
-FetchContent_Declare(
-  live555
-  URL ${LIVE555_URL}
-)
-FetchContent_MakeAvailable(live555)
-message("Content downloaded from ${LIVE555_URL} to ${live555_SOURCE_DIR}")
-
 ExternalProject_Add(cvi_rtsp
   PREFIX ${CMAKE_CURRENT_BINARY_DIR}/_deps/cvi_rtsp-Download
   GIT_REPOSITORY ssh://10.58.65.11:29418/cvi_rtsp
-  BUILD_COMMAND CROSS_COMPILE=${TC_PATH}${CROSS_COMPILE} LIVE555_DIR=${live555_SOURCE_DIR} ./build.sh
+  BUILD_COMMAND CROSS_COMPILE=${TC_PATH}${CROSS_COMPILE} ./build.sh
   CONFIGURE_COMMAND ""
   INSTALL_COMMAND ""
   BUILD_IN_SOURCE true
