f65160137e5036b21d0c826e44ab3ae74699feb0
Jeff Huang <jiaji.huang@cvitek.com>
2021-09-03 20:29:47 +0800

    [sample] fix failed to run sample_init
    Symptom: failed to run sample_init
    Root cause: CVI_AI_SetModelPath return CVI_FAILURE due to passing a fake path
    Solution: remove related code snippet from sample
    Note: N/A
    Change-Id: Idc006787d2cc347092d005e147b11a70b5a73958
    Reviewed-on: https://gerritmaster.cvitek.com:8087/c/cviai/+/14513
    Reviewed-by: <jiaji.huang@cvitek.com>

--- sample/sample_init.c
+++ sample/sample_init.c
@@ -10,27 +10,6 @@ int main(void) {
     printf("Handle create failed\n");
     return ret;
   }
-  // This is a fake path. CVI_AI_SetModelPath will not actually read a model, it'll just
-  // cache the path to the handle. By setting a fake path, we can see if the handle is
-  // created successfully.
-  const char fake_path[] = "face_quality.cvimodel";
-  if ((ret = CVI_AI_SetModelPath(handle, CVI_AI_SUPPORTED_MODEL_FACEQUALITY, fake_path)) !=
-      CVI_SUCCESS) {
-    printf("Set model path failed.\n");
-    return ret;
-  }
-  char *path = NULL;
-  if ((ret = CVI_AI_GetModelPath(handle, CVI_AI_SUPPORTED_MODEL_FACEQUALITY, &path)) !=
-      CVI_SUCCESS) {
-    printf("Get model path failed.\n");
-    return ret;
-  }
-  // Check if the two path are the same.
-  if (strcmp(path, fake_path) != 0) {
-    ret = CVI_FAILURE;
-  }
-  // Free the pointer created from CVI_AI_GetModelPath.
-  free(path);
 
   // Get the used group ids by AI SDK.
   VPSS_GRP *groups = NULL;
