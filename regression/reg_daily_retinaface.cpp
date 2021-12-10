a2db5e626e99df8eceeffde586caa44fb59b9bec
ryan.sun <ryan.sun@cvitek.com>
2021-12-10 17:45:38 +0800

    [unit tests] add hardhat / irfd, update retinaface daily regression
    Change-Id: I0ccf0bba516a02e3f8c7e24cfbe75b060267f401
    Reviewed-on: https://gerritmaster.cvitek.com:8087/c/cviai/+/19379
    Reviewed-by: <jiaji.huang@cvitek.com>

--- regression/reg_daily_retinaface.cpp
+++ regression/reg_daily_retinaface.cpp
@@ -27,9 +27,6 @@ class RetinafaceTestSuite : public CVIAIModelTestSuite {
 
  protected:
   virtual void SetUp() {
-    std::string model_name = std::string(m_json_object["model"]);
-    m_model_path = (m_model_dir / fs::path(model_name)).string();
-
     m_ai_handle = NULL;
     ASSERT_EQ(CVI_AI_CreateHandle2(&m_ai_handle, 1, 0), CVIAI_SUCCESS);
     ASSERT_EQ(CVI_AI_SetVpssTimeout(m_ai_handle, 1000), CVIAI_SUCCESS);
@@ -42,136 +39,168 @@ class RetinafaceTestSuite : public CVIAIModelTestSuite {
 };
 
 TEST_F(RetinafaceTestSuite, open_close_model) {
-  AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
-                         false);
-  ASSERT_NO_FATAL_FAILURE(aimodel.open());
+  for (size_t test_index = 0; test_index < m_json_object.size(); test_index++) {
+    std::string model_name = std::string(m_json_object[test_index]["model"]);
+    m_model_path = (m_model_dir / fs::path(model_name)).string();
 
-  const char *model_path_get = CVI_AI_GetModelPath(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE);
+    AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
+                           false);
+    ASSERT_NO_FATAL_FAILURE(aimodel.open());
 
-  EXPECT_PRED2([](auto s1, auto s2) { return s1 == s2; }, m_model_path,
-               std::string(model_path_get));
+    const char *model_path_get =
+        CVI_AI_GetModelPath(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE);
+
+    EXPECT_PRED2([](auto s1, auto s2) { return s1 == s2; }, m_model_path,
+                 std::string(model_path_get));
+  }
 }
 
 TEST_F(RetinafaceTestSuite, get_vpss_config) {
-  AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
-                         false);
-  ASSERT_NO_FATAL_FAILURE(aimodel.open());
-  cvai_vpssconfig_t vpssconfig;
-  vpssconfig.chn_attr.u32Height = 200;
-  vpssconfig.chn_attr.u32Width = 200;
-  vpssconfig.chn_attr.enPixelFormat = PIXEL_FORMAT_ARGB_1555;
-  vpssconfig.chn_attr.stNormalize.bEnable = false;
+  uint32_t dstWidth[3] = {608, 342, 608};
+  uint32_t dstHeight[3] = {608, 608, 342};
 
-  EXPECT_EQ(CVI_AI_GetVpssChnConfig(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, 342, 608, 0,
-                                    &vpssconfig),
-            CVIAI_SUCCESS);
+  for (size_t test_index = 0; test_index < m_json_object.size(); test_index++) {
+    std::string model_name = std::string(m_json_object[test_index]["model"]);
+    m_model_path = (m_model_dir / fs::path(model_name)).string();
 
-  uint32_t dstWidth = 342;
-  uint32_t dstHeight = 608;
-  float factor[3] = {0.50195837, 0.50195837, 0.50195837};
-  float mean[3] = {0.0, 0.0, 0.0};
+    AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
+                           false);
+    ASSERT_NO_FATAL_FAILURE(aimodel.open());
+    cvai_vpssconfig_t vpssconfig;
+    vpssconfig.chn_attr.u32Height = 200;
+    vpssconfig.chn_attr.u32Width = 200;
+    vpssconfig.chn_attr.enPixelFormat = PIXEL_FORMAT_ARGB_1555;
+    vpssconfig.chn_attr.stNormalize.bEnable = false;
 
-  EXPECT_EQ(vpssconfig.chn_attr.u32Width, dstWidth);
-  EXPECT_EQ(vpssconfig.chn_attr.u32Width, dstWidth);
-  EXPECT_EQ(vpssconfig.chn_attr.u32Height, dstHeight);
-  EXPECT_EQ(vpssconfig.chn_attr.enVideoFormat, VIDEO_FORMAT_LINEAR);
-  EXPECT_EQ(vpssconfig.chn_attr.enPixelFormat, PIXEL_FORMAT_RGB_888_PLANAR);
+    EXPECT_EQ(CVI_AI_GetVpssChnConfig(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, 342, 608, 0,
+                                      &vpssconfig),
+              CVIAI_SUCCESS);
 
-  for (uint32_t i = 0; i < 3; i++) {
-    EXPECT_FLOAT_EQ(vpssconfig.chn_attr.stNormalize.factor[i], factor[i]);
-  }
-  for (uint32_t i = 0; i < 3; i++) {
-    EXPECT_FLOAT_EQ(vpssconfig.chn_attr.stNormalize.mean[i], mean[i]);
+    float factor[3] = {0.50195837, 0.50195837, 0.50195837};
+    float mean[3] = {0.0, 0.0, 0.0};
+
+    uint32_t model_width = dstWidth[test_index];
+    uint32_t model_height = dstHeight[test_index];
+
+    EXPECT_EQ(vpssconfig.chn_attr.u32Width, model_width);
+    EXPECT_EQ(vpssconfig.chn_attr.u32Height, model_height);
+    EXPECT_EQ(vpssconfig.chn_attr.enVideoFormat, VIDEO_FORMAT_LINEAR);
+    EXPECT_EQ(vpssconfig.chn_attr.enPixelFormat, PIXEL_FORMAT_RGB_888_PLANAR);
+
+    for (uint32_t i = 0; i < 3; i++) {
+      EXPECT_FLOAT_EQ(vpssconfig.chn_attr.stNormalize.factor[i], factor[i]);
+    }
+    for (uint32_t i = 0; i < 3; i++) {
+      EXPECT_FLOAT_EQ(vpssconfig.chn_attr.stNormalize.mean[i], mean[i]);
+    }
   }
 }
 
 TEST_F(RetinafaceTestSuite, skip_vpss_preprocess) {
-  // select image_0 for test
-  std::string image_path = (m_image_dir / std::string(m_json_object["test_images"][0])).string();
+  for (size_t test_index = 0; test_index < m_json_object.size(); test_index++) {
+    std::string model_name = std::string(m_json_object[test_index]["model"]);
+    m_model_path = (m_model_dir / fs::path(model_name)).string();
 
-  Image frame(image_path, PIXEL_FORMAT_BGR_888);
-  ASSERT_TRUE(frame.open());
-
-  {
-    AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
-                           false);
-    ASSERT_NO_FATAL_FAILURE(aimodel.open());
-    cvai_face_t face_meta;
-    memset(&face_meta, 0, sizeof(cvai_face_t));
-    EXPECT_EQ(CVI_AI_RetinaFace(m_ai_handle, frame.getFrame(), &face_meta), CVIAI_SUCCESS);
-  }
-  {
-    AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
-                           true);
-    ASSERT_NO_FATAL_FAILURE(aimodel.open());
-    AIObject<cvai_face_t> face_meta;
-    init_face_meta(face_meta, 1);
-    EXPECT_EQ(CVI_AI_RetinaFace(m_ai_handle, frame.getFrame(), face_meta), CVIAI_ERR_INFERENCE);
-  }
-}
-
-TEST_F(RetinafaceTestSuite, inference) {
-  AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
-                         false);
-  ASSERT_NO_FATAL_FAILURE(aimodel.open());
-
-  for (int img_idx = 0; img_idx < 1; img_idx++) {
     // select image_0 for test
     std::string image_path =
-        (m_image_dir / std::string(m_json_object["test_images"][img_idx])).string();
+        (m_image_dir / std::string(m_json_object[test_index]["test_images"][0])).string();
+
+    Image frame(image_path, PIXEL_FORMAT_BGR_888);
+    ASSERT_TRUE(frame.open());
 
     {
-      Image frame(image_path, PIXEL_FORMAT_RGB_888_PLANAR);
-      ASSERT_TRUE(frame.open());
-
+      AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
+                             false);
+      ASSERT_NO_FATAL_FAILURE(aimodel.open());
       cvai_face_t face_meta;
       memset(&face_meta, 0, sizeof(cvai_face_t));
       EXPECT_EQ(CVI_AI_RetinaFace(m_ai_handle, frame.getFrame(), &face_meta), CVIAI_SUCCESS);
     }
-
     {
-      Image frame(image_path, PIXEL_FORMAT_BGR_888);
-      ASSERT_TRUE(frame.open());
+      AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
+                             true);
+      ASSERT_NO_FATAL_FAILURE(aimodel.open());
+      AIObject<cvai_face_t> face_meta;
+      init_face_meta(face_meta, 1);
+      EXPECT_EQ(CVI_AI_RetinaFace(m_ai_handle, frame.getFrame(), face_meta), CVIAI_ERR_INFERENCE);
+    }
+  }
+}
 
-      cvai_face_t face_meta;
-      memset(&face_meta, 0, sizeof(cvai_face_t));
-      EXPECT_EQ(CVI_AI_RetinaFace(m_ai_handle, frame.getFrame(), &face_meta), CVIAI_SUCCESS);
+TEST_F(RetinafaceTestSuite, inference) {
+  for (size_t test_index = 0; test_index < m_json_object.size(); test_index++) {
+    std::string model_name = std::string(m_json_object[test_index]["model"]);
+    m_model_path = (m_model_dir / fs::path(model_name)).string();
+
+    AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
+                           false);
+    ASSERT_NO_FATAL_FAILURE(aimodel.open());
+
+    for (int img_idx = 0; img_idx < 1; img_idx++) {
+      // select image_0 for test
+      std::string image_path =
+          (m_image_dir / std::string(m_json_object[test_index]["test_images"][img_idx])).string();
+
+      {
+        Image frame(image_path, PIXEL_FORMAT_RGB_888_PLANAR);
+        ASSERT_TRUE(frame.open());
+
+        cvai_face_t face_meta;
+        memset(&face_meta, 0, sizeof(cvai_face_t));
+        EXPECT_EQ(CVI_AI_RetinaFace(m_ai_handle, frame.getFrame(), &face_meta), CVIAI_SUCCESS);
+      }
+
+      {
+        Image frame(image_path, PIXEL_FORMAT_BGR_888);
+        ASSERT_TRUE(frame.open());
+
+        cvai_face_t face_meta;
+        memset(&face_meta, 0, sizeof(cvai_face_t));
+        EXPECT_EQ(CVI_AI_RetinaFace(m_ai_handle, frame.getFrame(), &face_meta), CVIAI_SUCCESS);
+      }
     }
   }
 }
 
 TEST_F(RetinafaceTestSuite, accruacy) {
-  AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
-                         false);
-  ASSERT_NO_FATAL_FAILURE(aimodel.open());
+  for (size_t test_index = 0; test_index < m_json_object.size(); test_index++) {
+    std::string model_name = std::string(m_json_object[test_index]["model"]);
+    m_model_path = (m_model_dir / fs::path(model_name)).string();
 
-  int img_num = int(m_json_object["test_images"].size());
-  float threshold = float(m_json_object["threshold"]);
+    AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE, m_model_path.c_str(),
+                           false);
+    ASSERT_NO_FATAL_FAILURE(aimodel.open());
 
-  for (int img_idx = 0; img_idx < img_num; img_idx++) {
-    // select image_0 for test
-    std::string image_path =
-        (m_image_dir / std::string(m_json_object["test_images"][img_idx])).string();
+    int img_num = int(m_json_object[test_index]["test_images"].size());
+    float threshold = float(m_json_object[test_index]["threshold"]);
 
-    Image frame(image_path, PIXEL_FORMAT_BGR_888);
-    ASSERT_TRUE(frame.open());
+    for (int img_idx = 0; img_idx < img_num; img_idx++) {
+      std::string image_path =
+          (m_image_dir / std::string(m_json_object[test_index]["test_images"][img_idx])).string();
 
-    cvai_face_t face_meta;
-    memset(&face_meta, 0, sizeof(cvai_face_t));
+      Image frame(image_path, PIXEL_FORMAT_BGR_888);
+      ASSERT_TRUE(frame.open());
 
-    { EXPECT_EQ(CVI_AI_RetinaFace(m_ai_handle, frame.getFrame(), &face_meta), CVIAI_SUCCESS); }
+      cvai_face_t face_meta;
+      memset(&face_meta, 0, sizeof(cvai_face_t));
 
-    for (uint32_t i = 0; i < face_meta.size; i++) {
-      float expected_res_x1 = float(m_json_object["expected_results"][img_idx][1][i][0]);
-      float expected_res_y1 = float(m_json_object["expected_results"][img_idx][1][i][1]);
-      float expected_res_x2 = float(m_json_object["expected_results"][img_idx][1][i][2]);
-      float expected_res_y2 = float(m_json_object["expected_results"][img_idx][1][i][3]);
+      { EXPECT_EQ(CVI_AI_RetinaFace(m_ai_handle, frame.getFrame(), &face_meta), CVIAI_SUCCESS); }
 
-      {
-        EXPECT_LT(abs(face_meta.info[i].bbox.x1 - expected_res_x1), threshold);
-        EXPECT_LT(abs(face_meta.info[i].bbox.y1 - expected_res_y1), threshold);
-        EXPECT_LT(abs(face_meta.info[i].bbox.x2 - expected_res_x2), threshold);
-        EXPECT_LT(abs(face_meta.info[i].bbox.y2 - expected_res_y2), threshold);
+      for (uint32_t i = 0; i < face_meta.size; i++) {
+        float expected_res_x1 =
+            float(m_json_object[test_index]["expected_results"][img_idx][1][i][0]);
+        float expected_res_y1 =
+            float(m_json_object[test_index]["expected_results"][img_idx][1][i][1]);
+        float expected_res_x2 =
+            float(m_json_object[test_index]["expected_results"][img_idx][1][i][2]);
+        float expected_res_y2 =
+            float(m_json_object[test_index]["expected_results"][img_idx][1][i][3]);
+        {
+          EXPECT_LT(abs(face_meta.info[i].bbox.x1 - expected_res_x1), threshold);
+          EXPECT_LT(abs(face_meta.info[i].bbox.y1 - expected_res_y1), threshold);
+          EXPECT_LT(abs(face_meta.info[i].bbox.x2 - expected_res_x2), threshold);
+          EXPECT_LT(abs(face_meta.info[i].bbox.y2 - expected_res_y2), threshold);
+        }
       }
     }
   }
--- regression/reg_daily_retinaface_hardhat.cpp
+++ regression/reg_daily_retinaface_hardhat.cpp
@@ -1,131 +1,194 @@
+#include <experimental/filesystem>
 #include <fstream>
+#include <memory>
 #include <string>
 #include "core/utils/vpss_helper.h"
 #include "cviai.h"
+#include "cviai_test.hpp"
 #include "evaluation/cviai_evaluation.h"
 #include "evaluation/cviai_media.h"
+#include "gtest.h"
 #include "json.hpp"
+#include "raii.hpp"
+#include "regression_utils.hpp"
 
-int main(int argc, char *argv[]) {
-  if (argc != 4) {
-    printf(
-        "Usage: %s <model_dir>\n"
-        "          <image_dir>\n"
-        "          <regression_json>\n",
-        argv[0]);
-    return CVI_FAILURE;
-  }
-  // CVI_S32 ret = CVI_SUCCESS;
-  std::string model_dir = std::string(argv[1]);
-  std::string image_dir = std::string(argv[2]);
+namespace fs = std::experimental::filesystem;
+namespace cviai {
+namespace unitest {
 
-  nlohmann::json m_json_read;
-  std::ofstream m_ofs_results;
+class RetinafaceHardhatTestSuite : public CVIAIModelTestSuite {
+ public:
+  RetinafaceHardhatTestSuite()
+      : CVIAIModelTestSuite("reg_daily_retinaface_hardhat.json", "reg_daily_retinaface_hardhat") {}
 
-  std::ifstream filestr(argv[3]);
-  filestr >> m_json_read;
-  filestr.close();
+  virtual ~RetinafaceHardhatTestSuite() = default;
 
-  std::string fd_model_name = std::string(m_json_read["model"]);
-  std::string fd_model_path = model_dir + "/" + fd_model_name;
-  printf("fd_model_path: %s\n", fd_model_path.c_str());
+  std::string m_model_path;
 
-  int img_num = int(m_json_read["test_images"].size());
-  printf("img_num: %d\n", img_num);
+ protected:
+  virtual void SetUp() {
+    std::string model_name = std::string(m_json_object["model"]);
+    m_model_path = (m_model_dir / fs::path(model_name)).string();
 
-  float threshold_bbox = float(m_json_read["threshold_bbox"]);
-  printf("threshold_bbox: %f\n", threshold_bbox);
-  float threshold_score = float(m_json_read["threshold_score"]);
-  printf("threshold_score: %f\n", threshold_score);
-
-  static CVI_S32 vpssgrp_width = 1920;
-  static CVI_S32 vpssgrp_height = 1080;
-  cviai_handle_t handle = NULL;
-
-  CVI_S32 ret = CVIAI_SUCCESS;
-  ret = MMF_INIT_HELPER2(vpssgrp_width, vpssgrp_height, PIXEL_FORMAT_RGB_888, 3, vpssgrp_width,
-                         vpssgrp_height, PIXEL_FORMAT_RGB_888, 3);
-  if (ret != CVIAI_SUCCESS) {
-    printf("Init sys failed with %#x!\n", ret);
-    return ret;
-  }
-  ret = CVI_AI_CreateHandle(&handle);
-  if (ret != CVIAI_SUCCESS) {
-    printf("Create handle failed with %#x!\n", ret);
-    return ret;
+    m_ai_handle = NULL;
+    ASSERT_EQ(CVI_AI_CreateHandle2(&m_ai_handle, 1, 0), CVIAI_SUCCESS);
+    ASSERT_EQ(CVI_AI_SetVpssTimeout(m_ai_handle, 1000), CVIAI_SUCCESS);
   }
 
-  ret = CVI_AI_OpenModel(handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE_HARDHAT, fd_model_path.c_str());
-  if (ret != CVIAI_SUCCESS) {
-    printf("Set model failed with %#x!\n", ret);
-    return ret;
+  virtual void TearDown() {
+    CVI_AI_DestroyHandle(m_ai_handle);
+    m_ai_handle = NULL;
   }
+};
 
-  CVI_AI_SetSkipVpssPreprocess(handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE_HARDHAT, false);
+TEST_F(RetinafaceHardhatTestSuite, open_close_model) {
+  AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE_HARDHAT,
+                         m_model_path.c_str(), false);
+  ASSERT_NO_FATAL_FAILURE(aimodel.open());
+
+  const char *model_path_get =
+      CVI_AI_GetModelPath(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE_HARDHAT);
+
+  EXPECT_PRED2([](auto s1, auto s2) { return s1 == s2; }, m_model_path,
+               std::string(model_path_get));
+}
+
+TEST_F(RetinafaceHardhatTestSuite, get_vpss_config) {
+  AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE_HARDHAT,
+                         m_model_path.c_str(), false);
+  ASSERT_NO_FATAL_FAILURE(aimodel.open());
+  cvai_vpssconfig_t vpssconfig;
+  vpssconfig.chn_attr.u32Height = 200;
+  vpssconfig.chn_attr.u32Width = 200;
+  vpssconfig.chn_attr.enPixelFormat = PIXEL_FORMAT_ARGB_1555;
+  vpssconfig.chn_attr.stNormalize.bEnable = false;
+
+  EXPECT_EQ(CVI_AI_GetVpssChnConfig(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE_HARDHAT, 342,
+                                    608, 0, &vpssconfig),
+            CVIAI_SUCCESS);
+
+  uint32_t dstWidth = 1280;
+  uint32_t dstHeight = 720;
+  float factor[3] = {0.84685433, 0.84685433, 0.84685433};
+  float mean[3] = {104.16309, 99.081955, 88.072853};
+
+  EXPECT_EQ(vpssconfig.chn_attr.u32Width, dstWidth);
+  EXPECT_EQ(vpssconfig.chn_attr.u32Width, dstWidth);
+  EXPECT_EQ(vpssconfig.chn_attr.u32Height, dstHeight);
+  EXPECT_EQ(vpssconfig.chn_attr.enVideoFormat, VIDEO_FORMAT_LINEAR);
+  EXPECT_EQ(vpssconfig.chn_attr.enPixelFormat, PIXEL_FORMAT_BGR_888_PLANAR);
+
+  for (uint32_t i = 0; i < 3; i++) {
+    EXPECT_FLOAT_EQ(vpssconfig.chn_attr.stNormalize.factor[i], factor[i]);
+  }
+  for (uint32_t i = 0; i < 3; i++) {
+    EXPECT_FLOAT_EQ(vpssconfig.chn_attr.stNormalize.mean[i], mean[i]);
+  }
+}
+
+TEST_F(RetinafaceHardhatTestSuite, skip_vpss_preprocess) {
+  // select image_0 for test
+  std::string image_path = (m_image_dir / std::string(m_json_object["test_images"][0])).string();
+
+  Image frame(image_path, PIXEL_FORMAT_BGR_888);
+  ASSERT_TRUE(frame.open());
+
+  {
+    AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE_HARDHAT,
+                           m_model_path.c_str(), false);
+    ASSERT_NO_FATAL_FAILURE(aimodel.open());
+    cvai_face_t face_meta;
+    memset(&face_meta, 0, sizeof(cvai_face_t));
+    EXPECT_EQ(CVI_AI_RetinaFace_Hardhat(m_ai_handle, frame.getFrame(), &face_meta), CVIAI_SUCCESS);
+  }
+  {
+    AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE_HARDHAT,
+                           m_model_path.c_str(), true);
+    ASSERT_NO_FATAL_FAILURE(aimodel.open());
+    AIObject<cvai_face_t> face_meta;
+    init_face_meta(face_meta, 1);
+    EXPECT_EQ(CVI_AI_RetinaFace_Hardhat(m_ai_handle, frame.getFrame(), face_meta),
+              CVIAI_ERR_INFERENCE);
+  }
+}
+
+TEST_F(RetinafaceHardhatTestSuite, inference) {
+  AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE_HARDHAT,
+                         m_model_path.c_str(), false);
+  ASSERT_NO_FATAL_FAILURE(aimodel.open());
+
+  for (int img_idx = 0; img_idx < 1; img_idx++) {
+    // select image_0 for test
+    std::string image_path =
+        (m_image_dir / std::string(m_json_object["test_images"][img_idx])).string();
+
+    {
+      Image frame(image_path, PIXEL_FORMAT_RGB_888_PLANAR);
+      ASSERT_TRUE(frame.open());
+
+      cvai_face_t face_meta;
+      memset(&face_meta, 0, sizeof(cvai_face_t));
+      EXPECT_EQ(CVI_AI_RetinaFace_Hardhat(m_ai_handle, frame.getFrame(), &face_meta),
+                CVIAI_SUCCESS);
+    }
+
+    {
+      Image frame(image_path, PIXEL_FORMAT_BGR_888);
+      ASSERT_TRUE(frame.open());
+
+      cvai_face_t face_meta;
+      memset(&face_meta, 0, sizeof(cvai_face_t));
+      EXPECT_EQ(CVI_AI_RetinaFace_Hardhat(m_ai_handle, frame.getFrame(), &face_meta),
+                CVIAI_SUCCESS);
+    }
+  }
+}
+
+TEST_F(RetinafaceHardhatTestSuite, accruacy) {
+  AIModelHandler aimodel(m_ai_handle, CVI_AI_SUPPORTED_MODEL_RETINAFACE_HARDHAT,
+                         m_model_path.c_str(), false);
+  ASSERT_NO_FATAL_FAILURE(aimodel.open());
+
+  int img_num = int(m_json_object["test_images"].size());
+  float threshold_bbox = float(m_json_object["threshold_bbox"]);
+  float threshold_score = float(m_json_object["threshold_score"]);
 
   for (int img_idx = 0; img_idx < img_num; img_idx++) {
-    std::string image_path = image_dir + "/" + std::string(m_json_read["test_images"][img_idx]);
-    printf("[%d] %s\n", img_idx, image_path.c_str());
+    // select image_0 for test
+    std::string image_path =
+        (m_image_dir / std::string(m_json_object["test_images"][img_idx])).string();
 
-    int expected_res_num = int(m_json_read["expected_results"][img_idx][0]);
-
-    VB_BLK blk1;
-    VIDEO_FRAME_INFO_S frame;
-    CVI_S32 ret = CVI_AI_ReadImage(image_path.c_str(), &blk1, &frame, PIXEL_FORMAT_BGR_888);
-    if (ret != CVIAI_SUCCESS) {
-      printf("Read image failed with %#x!\n", ret);
-      return ret;
-    }
+    Image frame(image_path, PIXEL_FORMAT_BGR_888);
+    ASSERT_TRUE(frame.open());
 
     cvai_face_t face_meta;
     memset(&face_meta, 0, sizeof(cvai_face_t));
 
-    CVI_AI_RetinaFace_Hardhat(handle, &frame, &face_meta);
-    // CVI_AI_Service_FaceDrawRect(NULL, &face_meta, &frame, false,
-    // CVI_AI_Service_GetDefaultBrush());
-    bool pass = (expected_res_num == int(face_meta.size));
-#if 0
-    printf("[%d] pass: %d; expected det nums: %d, result: %d\n", img_idx, pass, expected_res_num,
-           face_meta.size);
-#endif
-    if (!pass) {
-      return CVIAI_FAILURE;
+    {
+      EXPECT_EQ(CVI_AI_RetinaFace_Hardhat(m_ai_handle, frame.getFrame(), &face_meta),
+                CVIAI_SUCCESS);
     }
 
     for (uint32_t i = 0; i < face_meta.size; i++) {
-      float expected_res_x1 = float(m_json_read["expected_results"][img_idx][1][i][0]);
-      float expected_res_y1 = float(m_json_read["expected_results"][img_idx][1][i][1]);
-      float expected_res_x2 = float(m_json_read["expected_results"][img_idx][1][i][2]);
-      float expected_res_y2 = float(m_json_read["expected_results"][img_idx][1][i][3]);
-      float expected_res_bbox_conf = float(m_json_read["expected_results"][img_idx][1][i][4]);
-      float expected_res_hardhat_score = float(m_json_read["expected_results"][img_idx][1][i][5]);
+      float expected_res_x1 = float(m_json_object["expected_results"][img_idx][1][i][0]);
+      float expected_res_y1 = float(m_json_object["expected_results"][img_idx][1][i][1]);
+      float expected_res_x2 = float(m_json_object["expected_results"][img_idx][1][i][2]);
+      float expected_res_y2 = float(m_json_object["expected_results"][img_idx][1][i][3]);
+      float expected_res_bbox_conf = float(m_json_object["expected_results"][img_idx][1][i][4]);
+      float expected_res_hardhat_score = float(m_json_object["expected_results"][img_idx][1][i][5]);
 
-      bool pass =
-          (abs(face_meta.info[i].bbox.x1 - expected_res_x1) < threshold_bbox) &
-          (abs(face_meta.info[i].bbox.y1 - expected_res_y1) < threshold_bbox) &
-          (abs(face_meta.info[i].bbox.x2 - expected_res_x2) < threshold_bbox) &
-          (abs(face_meta.info[i].bbox.y2 - expected_res_y2) < threshold_bbox) &
-          (abs(face_meta.info[i].bbox.score - expected_res_bbox_conf) < threshold_score) &
-          (abs(face_meta.info[i].hardhat_score - expected_res_hardhat_score) < threshold_score);
-
-#if 0
-      printf(
-          "[%d][%d] pass: %d, x1, y1, x2, y2, bbox_conf, hardhat_score, \n expected : [%f, %f, %f, %f, %f, %f], result : [%f, %f, %f, %f, %f, %f]\n",
-          img_idx, i, pass, expected_res_x1, expected_res_y1, expected_res_x2, expected_res_y2,
-          expected_res_bbox_conf, expected_res_hardhat_score,
-          face_meta.info[i].bbox.x1, face_meta.info[i].bbox.y1, face_meta.info[i].bbox.x2,
-          face_meta.info[i].bbox.y2, face_meta.info[i].bbox.score, face_meta.info[i].hardhat_score);
-#endif
-      if (!pass) {
-        return CVIAI_FAILURE;
+      {
+        EXPECT_LT(abs(face_meta.info[i].bbox.x1 - expected_res_x1), threshold_bbox);
+        EXPECT_LT(abs(face_meta.info[i].bbox.y1 - expected_res_y1), threshold_bbox);
+        EXPECT_LT(abs(face_meta.info[i].bbox.x2 - expected_res_x2), threshold_bbox);
+        EXPECT_LT(abs(face_meta.info[i].bbox.y2 - expected_res_y2), threshold_bbox);
+        EXPECT_LT(abs(face_meta.info[i].bbox.score - expected_res_bbox_conf), threshold_score);
+        EXPECT_LT(abs(face_meta.info[i].hardhat_score - expected_res_hardhat_score),
+                  threshold_score);
       }
     }
-
-    CVI_AI_Free(&face_meta);
-    CVI_VB_ReleaseBlock(blk1);
   }
-  CVI_AI_DestroyHandle(handle);
-  CVI_SYS_Exit();
-  printf("retinaface hardhat regression result: all pass\n");
-  return CVIAI_SUCCESS;
 }
+
+}  // namespace unitest
+}  // namespace cviai
