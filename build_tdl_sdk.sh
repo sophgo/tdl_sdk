50a4d14b9bb671dcf43f90f422fa124a6e4cd8a0
zhenxing.ye <zhenxing.ye@sophgo.com>
2025-03-07 17:40:45 +0800

    build_tdl_sdk.sh support BM1688

--- build_tdl_sdk.sh
+++ build_tdl_sdk.sh
@@ -2,57 +2,47 @@
 
 # print usage
 print_usage() {
-    echo "Usage: source ${BASH_SOURCE[0]} [option] [type]"
-    echo "Options:"
-    echo "  (no option) Build modules only"
-    echo "  sample      Build samples only"
-    echo "  all         Build both modules and sample"
+    echo "Usage: source ${BASH_SOURCE[0]} build_type"
+    echo "build_type:"
+    echo "  all: Build 181x and a2"
+    echo "  BM1688: Build BM1688 "
 }
 
 # Check parameter
-if [ "$#" -gt 2 ]; then
-    echo "Error: Too many arguments"
-    print_usage
-    return 1
-fi
-
-# get tdl_sdk root dir
-CVI_TDL_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TMP_WORKING_DIR="$CVI_TDL_ROOT"/tmp"$CHIP_ARCH"
-BUILD_WORKING_DIR="$TMP_WORKING_DIR"/build_sdk
-BUILD_DOWNLOAD_DIR="$TMP_WORKING_DIR"/_deps
-TDL_SDK_INSTALL_PATH="$CVI_TDL_ROOT"/install"$CHIP_ARCH"
-
-# Set build opetion and type
-BUILD_OPTION=
-BUILD_TYPE=SDKRelease
-if [ "$#" -eq 0 ]; then
-    echo "Using ./${BASH_SOURCE[0]}"
-    echo "Compiling modules..."
-elif [ "$1" = "sample" ]; then
-    echo "Using ./${BASH_SOURCE[0]} sample"
-    echo "Compiling sample..."
-    BUILD_OPTION=sample
-elif [ "$1" = "all" ]; then
-    echo "Using ./${BASH_SOURCE[0]} all"
-    echo "Compiling modules and sample..."
-    BUILD_OPTION=all
-elif [ "$1" = "clean" ]; then
-    echo "Using ./${BASH_SOURCE[0]} clean"
-    echo "Cleaning build..."
-    if [ -d "${BUILD_DOWNLOAD_DIR}" ]; then
-        echo "Cleanup tmp folder."
-        rm -rf $BUILD_DOWNLOAD_DIR
-    fi
-    if [ -d "${TDL_SDK_INSTALL_PATH}" ]; then
-        echo "Cleanup install folder."
-        rm -rf $TDL_SDK_INSTALL_PATH
-    fi
-    exit 0
-else
-    echo "Error: Invalid option"
+if [ "$1" != "all" -a "$1" != "BM1688" ]; then
+    echo "Error  arguments"
     print_usage
     exit 1
+fi
+
+if [[ "$1" == "BM1688" ]]; then
+    CHIP_ARCH=$1
+fi
+
+
+# get tdl_sdk root dir
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CVI_TDL_ROOT=$(readlink -f $SCRIPT_DIR)
+
+BUILD_WORKING_DIR=$CVI_TDL_ROOT/build$CHIP_ARCH
+BUILD_DOWNLOAD_DIR="$BUILD_WORKING_DIR"/_deps
+TDL_SDK_INSTALL_PATH="$CVI_TDL_ROOT"/install/"$CHIP_ARCH"
+
+
+# Set build opetion and type
+BUILD_TYPE=Debug
+
+
+if [ -d "$BUILD_DOWNLOAD_DIR" ]; then
+    echo "BUILD_DOWNLOAD_DIR=$BUILD_DOWNLOAD_DIR"
+else
+    mkdir -p $BUILD_DOWNLOAD_DIR
+fi
+
+if [ -d "$TDL_SDK_INSTALL_PATH" ]; then
+    echo "TDL_SDK_INSTALL_PATH=$TDL_SDK_INSTALL_PATH"
+else
+    mkdir -p $TDL_SDK_INSTALL_PATH
 fi
 
 # check system type
@@ -64,24 +54,29 @@ fi
 # set ftp server
 FTP_SERVER_IP=${FTP_SERVER_IP:-10.80.0.5}
 
-# set host-tool
-HOST_TOOL_PATH="$CROSS_COMPILE_PATH"
-TARGET_MACHINE="$(${CROSS_COMPILE}gcc -dumpmachine)"
-TOOLCHAIN_FILE="$CVI_TDL_ROOT"/toolchain/"$TARGET_MACHINE".cmake
+echo $CHIP_ARCH
+if [[ "$CHIP_ARCH" == "BM1688" ]]; then
 
-# set dependency
-MW_VER=v2
-MPI_PATH="$TOP_DIR"/cvi_mpi
-TPU_SDK_INSTALL_PATH="$OUTPUT_DIR"/tpu_"$SDK_VER"/cvitek_tpu_sdk
-IVE_SDK_INSTALL_PATH="$OUTPUT_DIR"/tpu_"$SDK_VER"/cvitek_ive_sdk
+    HOST_TOOL_PATH=$CVI_TDL_ROOT/../host-tools/gcc/gcc-buildroot-9.3.0-aarch64-linux-gnu
 
-# init build working dir
-if [ -d "${BUILD_WORKING_DIR}" ]; then
-    echo "Cleanup tmp folder."
-    rm -rf $BUILD_WORKING_DIR
+    OPENCV_ROOT_DIR=$CVI_TDL_ROOT/sophon_sdk/sophon-opencv_1.8.0
+    MLIR_SDK_ROOT=$CVI_TDL_ROOT/sophon_sdk/libsophon-0.4.10
+    MIDDLEWARE_ROOT_DIR=$CVI_TDL_ROOT/sophon_sdk/sophon-ffmpeg_1.8.0
+    ISP_ROOT_DIR=$CVI_TDL_ROOT/sophon_sdk/sophon-soc-libisp_1.0.0
+    TOOLCHAIN_FILE=$CVI_TDL_ROOT/toolchain/toolchain930-aarch64-linux.cmake
+
+else
+    HOST_TOOL_PATH="$CROSS_COMPILE_PATH"
+    TPU_SDK_INSTALL_PATH="$OUTPUT_DIR"/tpu_"$SDK_VER"/cvitek_tpu_sdk
+    IVE_SDK_INSTALL_PATH="$OUTPUT_DIR"/tpu_"$SDK_VER"/cvitek_ive_sdk
+    TARGET_MACHINE="$(${CROSS_COMPILE}gcc -dumpmachine)"
+    TOOLCHAIN_FILE="$CVI_TDL_ROOT"/toolchain/"$TARGET_MACHINE".cmake
+
+    MW_VER=v2
+    MPI_PATH="$TOP_DIR"/cvi_mpi
+    BUILD_OPTION=$1
 fi
-echo "Creating tmp working directory."
-mkdir -p $BUILD_WORKING_DIR
+
 
 # into tmp/build_sdk
 pushd $BUILD_WORKING_DIR
@@ -115,29 +110,57 @@ elif [[ "$CHIP_ARCH" == "CV180X" ]]; then
 elif [[ "$CHIP_ARCH" == "SOPHON" ]]; then
     MPI_PATH="$TOP_DIR"/middleware/"$MW_VER"
     USE_TPU_IVE=OFF
+elif [[ "$CHIP_ARCH" == "BM1688" ]]; then
+    USE_TPU_IVE=OFF
 else
     echo "Unsupported chip architecture: ${CHIP_ARCH}"
     exit 1
 fi
 
-# build start
-$CMAKE_BIN -G Ninja $CVI_TDL_ROOT -DCVI_PLATFORM=$CHIP_ARCH \
-                                  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-                                  -DENABLE_CVI_TDL_CV_UTILS=ON \
-                                  -DMLIR_SDK_ROOT=$TPU_SDK_INSTALL_PATH \
-                                  -DMIDDLEWARE_SDK_ROOT=$MPI_PATH \
-                                  -DTPU_IVE_SDK_ROOT=$IVE_SDK_INSTALL_PATH \
-                                  -DCMAKE_INSTALL_PREFIX=$TDL_SDK_INSTALL_PATH \
-                                  -DTOOLCHAIN_ROOT_DIR=$HOST_TOOL_PATH \
-                                  -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE \
-                                  -DKERNEL_ROOT=$KERNEL_ROOT \
-                                  -DUSE_TPU_IVE=$USE_TPU_IVE \
-                                  -DBUILD_DOWNLOAD_DIR=$BUILD_DOWNLOAD_DIR \
-                                  -DCONFIG_DUAL_OS=$CONFIG_DUAL_OS \
-                                  -DBUILD_OPTION=$BUILD_OPTION \
-                                  -DTARGET_MACHINE=$TARGET_MACHINE \
-                                  -DMW_VER=$MW_VER \
-                                  -DFTP_SERVER_IP=$FTP_SERVER_IP
+
+if [[ "$CHIP_ARCH" != "BM1688" ]]; then
+
+    # build start
+    $CMAKE_BIN -G Ninja $CVI_TDL_ROOT \
+            -DCVI_PLATFORM=$CHIP_ARCH \
+            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+            -DENABLE_CVI_TDL_CV_UTILS=ON \
+            -DMLIR_SDK_ROOT=$TPU_SDK_INSTALL_PATH \
+            -DMIDDLEWARE_SDK_ROOT=$MPI_PATH \
+            -DTPU_IVE_SDK_ROOT=$IVE_SDK_INSTALL_PATH \
+            -DCMAKE_INSTALL_PREFIX=$TDL_SDK_INSTALL_PATH \
+            -DTOOLCHAIN_ROOT_DIR=$HOST_TOOL_PATH \
+            -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE \
+            -DKERNEL_ROOT=$KERNEL_ROOT \
+            -DUSE_TPU_IVE=$USE_TPU_IVE \
+            -DBUILD_DOWNLOAD_DIR=$BUILD_DOWNLOAD_DIR \
+            -DCONFIG_DUAL_OS=$CONFIG_DUAL_OS \
+            -DBUILD_OPTION=$BUILD_OPTION \
+            -DTARGET_MACHINE=$TARGET_MACHINE \
+            -DMW_VER=$MW_VER \
+            -DFTP_SERVER_IP=$FTP_SERVER_IP
+
+
+else
+
+    $CMAKE_BIN -G Ninja $CVI_TDL_ROOT \
+            -DCVI_PLATFORM=$CHIP_ARCH \
+            -DCVI_SYSTEM_PROCESSOR=$SYSTEM_PROCESSOR \
+            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+            -DMLIR_SDK_ROOT=$MLIR_SDK_ROOT \
+            -DMIDDLEWARE_SDK_ROOT=$MIDDLEWARE_ROOT_DIR \
+            -DISP_ROOT_DIR=$ISP_ROOT_DIR \
+            -DOPENCV_ROOT_DIR=$OPENCV_ROOT_DIR \
+            -DCMAKE_INSTALL_PREFIX=$TDL_SDK_INSTALL_PATH \
+            -DTOOLCHAIN_ROOT_DIR=$HOST_TOOL_PATH \
+            -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE \
+            -DKERNEL_ROOT=$KERNEL_ROOT \
+            -DUSE_TPU_IVE=$USE_TPU_IVE \
+            -DBUILD_DOWNLOAD_DIR=$BUILD_DOWNLOAD_DIR \
+            -DCONFIG_DUAL_OS=$CONFIG_DUAL_OS \
+            -DFTP_SERVER_IP=$FTP_SERVER_IP
+
+fi
 
 test $? -ne 0 && echo "cmake tdl_sdk failed !!" && popd && exit 1
 
