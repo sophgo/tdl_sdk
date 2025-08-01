56b1989fd5d650a3664cd6ae5be05543e0fa3935
junfei.liu <junfei.liu@sophgo.com>
2025-08-01 07:00:39 +0000

    [fix] add \n in log system.
    Change-Id: Id3d103362cb63b2ccde8dbc7c0765663f1bf9525
    Reviewed-on: https://gerrit-ai.sophgo.vip:8443/147949
    Reviewed-by: 振兴 叶 <zhenxing.ye@sophgo.com>

--- include/framework/utils/tdl_log.hpp
+++ include/framework/utils/tdl_log.hpp
@@ -13,38 +13,38 @@
 #ifdef LOGD
 #undef LOGD
 #endif
-#define LOGD(fmt, ...)                                                  \
-  ulog(LOG_DEBUG, "TDLSDK", ULOG_TAG, "[%s:%d] [D] " fmt, __FILENAME__, \
+#define LOGD(fmt, ...)                                                    \
+  ulog(LOG_DEBUG, "TDLSDK", ULOG_TAG, "[%s:%d] [D] \n" fmt, __FILENAME__, \
        __LINE__, ##__VA_ARGS__)
 #ifdef LOGI
 #undef LOGI
 #endif
-#define LOGI(fmt, ...)                                                 \
-  ulog(LOG_INFO, "TDLSDK", ULOG_TAG, "[%s:%d] [I] " fmt, __FILENAME__, \
+#define LOGI(fmt, ...)                                                   \
+  ulog(LOG_INFO, "TDLSDK", ULOG_TAG, "[%s:%d] [I] \n" fmt, __FILENAME__, \
        __LINE__, ##__VA_ARGS__)
 #ifdef LOGN
 #undef LOGN
 #endif
-#define LOGN(fmt, ...)                                                   \
-  ulog(LOG_NOTICE, "TDLSDK", ULOG_TAG, "[%s:%d] [N] " fmt, __FILENAME__, \
+#define LOGN(fmt, ...)                                                     \
+  ulog(LOG_NOTICE, "TDLSDK", ULOG_TAG, "[%s:%d] [N] \n" fmt, __FILENAME__, \
        __LINE__, ##__VA_ARGS__)
 #ifdef LOGW
 #undef LOGW
 #endif
-#define LOGW(fmt, ...)                                                    \
-  ulog(LOG_WARNING, "TDLSDK", ULOG_TAG, "[%s:%d] [W] " fmt, __FILENAME__, \
+#define LOGW(fmt, ...)                                                      \
+  ulog(LOG_WARNING, "TDLSDK", ULOG_TAG, "[%s:%d] [W] \n" fmt, __FILENAME__, \
        __LINE__, ##__VA_ARGS__)
 #ifdef LOGE
 #undef LOGE
 #endif
-#define LOGE(fmt, ...)                                                \
-  ulog(LOG_ERR, "TDLSDK", ULOG_TAG, "[%s:%d] [E] " fmt, __FILENAME__, \
+#define LOGE(fmt, ...)                                                  \
+  ulog(LOG_ERR, "TDLSDK", ULOG_TAG, "[%s:%d] [E] \n" fmt, __FILENAME__, \
        __LINE__, ##__VA_ARGS__)
 #ifdef LOGC
 #undef LOGC
 #endif
-#define LOGC(fmt, ...)                                                 \
-  ulog(LOG_CRIT, "TDLSDK", ULOG_TAG, "[%s:%d] [C] " fmt, __FILENAME__, \
+#define LOGC(fmt, ...)                                                   \
+  ulog(LOG_CRIT, "TDLSDK", ULOG_TAG, "[%s:%d] [C] \n" fmt, __FILENAME__, \
        __LINE__, ##__VA_ARGS__)
 #ifndef syslog
 #define syslog(level, fmt, ...) printf(fmt, ##__VA_ARGS__)
@@ -65,41 +65,41 @@
 
 #else
 
-#define LOGD(fmt, ...)                                                        \
-  syslog(TDL_LOG_CHN | LOG_DEBUG, "[%s:%d] [D] " fmt, __FILENAME__, __LINE__, \
-         ##__VA_ARGS__)
-#define LOGI(fmt, ...)                                                       \
-  syslog(TDL_LOG_CHN | LOG_INFO, "[%s:%d] [I] " fmt, __FILENAME__, __LINE__, \
-         ##__VA_ARGS__)
-#define LOGN(fmt, ...)                                                         \
-  syslog(TDL_LOG_CHN | LOG_NOTICE, "[%s:%d] [N] " fmt, __FILENAME__, __LINE__, \
-         ##__VA_ARGS__)
-#define LOGW(fmt, ...)                                                \
-  syslog(TDL_LOG_CHN | LOG_WARNING, "[%s:%d] [W] " fmt, __FILENAME__, \
+#define LOGD(fmt, ...)                                                   \
+  syslog(TDL_LOG_CHN | LOG_DEBUG, "[%s:%d] [D] " fmt "\n", __FILENAME__, \
          __LINE__, ##__VA_ARGS__)
-#define LOGE(fmt, ...)                                                      \
-  syslog(TDL_LOG_CHN | LOG_ERR, "[%s:%d] [E] " fmt, __FILENAME__, __LINE__, \
-         ##__VA_ARGS__)
-#define LOGC(fmt, ...)                                                       \
-  syslog(TDL_LOG_CHN | LOG_CRIT, "[%s:%d] [C] " fmt, __FILENAME__, __LINE__, \
-         ##__VA_ARGS__)
+#define LOGI(fmt, ...)                                                  \
+  syslog(TDL_LOG_CHN | LOG_INFO, "[%s:%d] [I] " fmt "\n", __FILENAME__, \
+         __LINE__, ##__VA_ARGS__)
+#define LOGN(fmt, ...)                                                    \
+  syslog(TDL_LOG_CHN | LOG_NOTICE, "[%s:%d] [N] " fmt "\n", __FILENAME__, \
+         __LINE__, ##__VA_ARGS__)
+#define LOGW(fmt, ...)                                                     \
+  syslog(TDL_LOG_CHN | LOG_WARNING, "[%s:%d] [W] " fmt "\n", __FILENAME__, \
+         __LINE__, ##__VA_ARGS__)
+#define LOGE(fmt, ...)                                                 \
+  syslog(TDL_LOG_CHN | LOG_ERR, "[%s:%d] [E] " fmt "\n", __FILENAME__, \
+         __LINE__, ##__VA_ARGS__)
+#define LOGC(fmt, ...)                                                  \
+  syslog(TDL_LOG_CHN | LOG_CRIT, "[%s:%d] [C] " fmt "\n", __FILENAME__, \
+         __LINE__, ##__VA_ARGS__)
 
 #endif
 
 #undef LOGE
-#define LOGE(fmt, ...)                                                        \
-  do {                                                                        \
-    syslog(TDL_LOG_CHN | LOG_ERR, "[%s:%d] [E] " fmt, __FILENAME__, __LINE__, \
-           ##__VA_ARGS__);                                                    \
-    fprintf(stderr, "[%s:%d] [E] " fmt "\n", __FILENAME__, __LINE__,          \
-            ##__VA_ARGS__);                                                   \
+#define LOGE(fmt, ...)                                                   \
+  do {                                                                   \
+    syslog(TDL_LOG_CHN | LOG_ERR, "[%s:%d] [E] " fmt "\n", __FILENAME__, \
+           __LINE__, ##__VA_ARGS__);                                     \
+    fprintf(stderr, "[%s:%d] [E] " fmt "\n", __FILENAME__, __LINE__,     \
+            ##__VA_ARGS__);                                              \
   } while (0)
-#define LOGIP(fmt, ...)                                                        \
-  do {                                                                         \
-    syslog(TDL_LOG_CHN | LOG_INFO, "[%s:%d] [E] " fmt, __FILENAME__, __LINE__, \
-           ##__VA_ARGS__);                                                     \
-    fprintf(stderr, "[%s:%d] [I] " fmt "\n", __FILENAME__, __LINE__,           \
-            ##__VA_ARGS__);                                                    \
+#define LOGIP(fmt, ...)                                                   \
+  do {                                                                    \
+    syslog(TDL_LOG_CHN | LOG_INFO, "[%s:%d] [I] " fmt "\n", __FILENAME__, \
+           __LINE__, ##__VA_ARGS__);                                      \
+    fprintf(stderr, "[%s:%d] [I] " fmt "\n", __FILENAME__, __LINE__,      \
+            ##__VA_ARGS__);                                               \
   } while (0)
 #endif
 #endif
